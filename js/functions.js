f4fbf3bf60dbb52fac6fece8fe38d98e7ce458a1
lilyanovi <zauzolkova.lilia@ya.ru>
2023-10-25 08:59:19 +0300

    no console

--- js/functions.js
+++ js/functions.js
@@ -126,9 +126,8 @@ const checkWorkingTimeLimits = (startWorkTime, endWorkTime, startMeetingTime, me
   return (startWorkTimeDate <= startMeetingTimeDate && (startMeetingTimeDate + meetingDurationTime) <= endWorkTimeDate);
 };
 
-
-console.log(checkWorkingTimeLimits('08:00', '17:30', '14:00', 90)); // true
-console.log(checkWorkingTimeLimits('8:0', '10:0', '8:0', 120)); // true
-console.log(checkWorkingTimeLimits('08:00', '14:30', '14:00', 90)); // false
-console.log(checkWorkingTimeLimits('14:00', '17:30', '08:0', 90)); // false
-console.log(checkWorkingTimeLimits('8:00', '17:30', '08:00', 900)); // false
+checkWorkingTimeLimits('08:00', '17:30', '14:00', 90); // true
+//console.log(checkWorkingTimeLimits('8:0', '10:0', '8:0', 120)); // true
+//console.log(checkWorkingTimeLimits('08:00', '14:30', '14:00', 90)); // false
+//console.log(checkWorkingTimeLimits('14:00', '17:30', '08:0', 90)); // false
+//console.log(checkWorkingTimeLimits('8:00', '17:30', '08:00', 900)); // false
